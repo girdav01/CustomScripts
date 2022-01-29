71bbd349130bf1fbb65f98ed604f4a48d7a3d392
David Girard <25532812+girdav01@users.noreply.github.com>
2022-01-28 23:41:59 -0500

    Add files via upload

---
+++ win/RunCommands.ps1
@@ -0,0 +1,18 @@
+﻿# Run any Powershell Expression
+# Trend Micro Vision One Custom Script Example with parameters
+# D. Girard Vision One PM Team
+# To run any commands from Vision One Custom Script Parameter use this
+# it could be Windows commands or pure powershell. results will be wrote to V1 results 
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/invoke-command?view=powershell-7.2
+# -expression "ping 192.168.1.1"
+# -expression "Get-Service"
+# -expression "Get-ChildItem c:\ir, *.*"
+# -expression "Invoke-WMIMethod -Class Win32_Process -Name Create -ArgumentList Notepad.exe"
+param(
+[Parameter (Mandatory = $true)] [String]$expression
+) 
+# need to convert string to script and add | Out-String to nicely format /n
+$scriptBlock = [Scriptblock]::Create($expression+"| Out-String")
+Write-host "RunCommands.ps1 -expression "$expression
+$results= Invoke-Command -ScriptBlock $scriptBlock
+Write-host $results
---
+++ win/StopService.ps1
@@ -0,0 +1,22 @@
+﻿# Stop Service by process name or process id
+# Trend Micro Vision One Custom Script Example with parameters
+# Note: Kill process in Vision One use the SHA1 of the EXE responsible for the process. this script use 2 different methods
+# D. Girard Vision One PM Team
+# To kill a service by service name or Display name:
+# -pName "Spooler" -by "Name"
+# -pName "Print Spooler" -by "DisplayName"
+param(
+[Parameter (Mandatory = $true)] [String]$pName,
+[Parameter (Mandatory = $true)] [ValidateSet("Name","DisplayName")][String]$by = "Name"
+)
+if ($pName) {
+    Write-host "StopService.ps1" $pName
+    if ($by -eq "Name") {
+        Stop-Service -Name $pName
+    }
+    elseif($by -eq "DisplayName") {
+        Stop-Service -DisplayName $pName
+    }
+    else {Write-host "action not recognized, please check misspelled action" $action}
+    Write-Host 'Service Stopped:' $pName
+}
